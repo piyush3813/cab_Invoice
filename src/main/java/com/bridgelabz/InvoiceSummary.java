03a11aa833c7e33e7051fa24d2bdde373b39b2ae
Piyush Mishra <piyushmishra.p@gmail.com>
2022-06-01 23:03:23 +0530

    added: enhanced class to provide invoice summary

---
+++ src/main/java/com/bridgelabz/InvoiceSummary.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+public class InvoiceSummary {
+    public int numberOfRides;
+    public double totalFare;
+    public double average;
+
+    public InvoiceSummary(int numberOfRides, double totalFare) {
+        this.numberOfRides = numberOfRides;
+        this.totalFare = totalFare;
+        this.average = average;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceSummary{" +
+                "numberOfRides=" + numberOfRides +
+                ", totalFare=" + totalFare +
+                ", avarage=" + average +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        InvoiceSummary other = (InvoiceSummary) obj;
+        return Double.doubleToLongBits(average) == Double.doubleToLongBits(other.average)
+                && numberOfRides == other.numberOfRides
+                && Double.doubleToLongBits(totalFare) == Double.doubleToLongBits(other.totalFare);
+    }
+}
